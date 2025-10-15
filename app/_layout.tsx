1fd3a4427710f7e0004da9e30fccc922893715cc
Luke Dowling <lpdowling26@googlemail.com>
2025-10-15 11:25:41 +0200

    chore: delete app reset

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,38 +1,38 @@
-import "../global.css";
+import "../global.css"
 
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from "@react-navigation/native";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
+} from "@react-navigation/native"
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
+import "react-native-reanimated"
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useColorScheme } from "@/hooks/use-color-scheme"
 
 export const unstable_settings = {
   anchor: "(tabs)",
-};
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         <Stack.Screen
-          name="index"
+          name='index'
           options={{ title: "Home", headerShown: false }}
         />
-        <Stack.Screen name="contact" options={{ title: "Contact Us" }} />
+        <Stack.Screen name='contact' options={{ title: "Contact Us" }} />
         <Stack.Screen
-          name="modal"
+          name='modal'
           options={{ presentation: "modal", title: "Modal" }}
         />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </ThemeProvider>
-  );
+  )
 }
